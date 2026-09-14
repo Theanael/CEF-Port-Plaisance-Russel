7cd0033ba012970b6a0817c221a17a7fc5572814
Theanael <theanael@gmail.com>
2026-09-14 15:25:14 +0200

    Added Catway Routes

---
+++ routes/catways.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+const APIcontroller = require('../controllers/api/catways');
+const dashboardController = require('../controllers/dashboard/catways');
+
+// Dashboard Routes
+router.get('/list',dashboardController.getList);
+router.get('/page/:id',dashboardController.getPage);
+router.get('/create',dashboardController.create);
+router.post('/add',dashboardController.add);
+router.get('/delete/:id',dashboardController.delete);
+router.post('/:id',dashboardController.updateState);
+
+
+// API Routes
+router.get('/',APIcontroller.getAll);
+router.get('/:id',APIcontroller.getOneById);
+router.post('/',APIcontroller.add);
+router.put('/:id',APIcontroller.updateState);
+router.delete('/:id',APIcontroller.delete);
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const catwayRoutes= require('./catways')
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.use('/catways', catwayRoutes)
 module.exports = router;
