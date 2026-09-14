5ee5153493d31deef67aba472491460e4444a644
Theanael <theanael@gmail.com>
2026-09-14 14:56:46 +0200

    Added Catway updateState controller

--- controllers/dashboard/catways.js
+++ controllers/dashboard/catways.js
@@ -5,18 +5,35 @@ exports.getList = async (req,res) => {
         const catways= await service.getAll();
         return res.render('catways/list', {catways:catways});
     } catch (error) {
-        return res.render('error',{error:error})
-    }
-}
+        return res.render('error',{message:error.message,error:error})
+    };
+};
 
 exports.getPage = async (req,res) => {
     const id = req.params.id
+    
     try {
         const catway = await service.getOneById(id);
         if (!catway) {
-            return res.render('error', {error:{}});
-        }
+            return res.render('error', {message:"Catway Not Found",error:{status:404}});
+        };
         return res.render('catways/page', {catway:catway});
+    } catch (error) {
+        return res.render('error',{message:error.message,error:error})
+    };
+};
+
+exports.updateState = async (req,res) => {
+    const id = req.params.id
+    const newState = req.body.catwayState
+    try {
+        const catway = await service.getOneById(id)
+        if (!catway){
+            return res.render('error', {message:"Catway Not Found",error:{status:404}});
+        }
+        await service.updateState(id,newState)
+        return res.redirect('/catways/page/'+id)
+
     } catch (error) {
         return res.render('error',{error:error})
     }
