daaa1f6435668853d39da8d96b80e9a6fcffa7d4
Theanael <theanael@gmail.com>
2026-09-14 14:44:55 +0200

    Added Catway Page and EJS Template

--- controllers/dashboard/catways.js
+++ controllers/dashboard/catways.js
@@ -1,10 +1,23 @@
 const service = require('../../services/catways')
 
-exports.getList= async (req,res) => {
+exports.getList = async (req,res) => {
     try {
-        const catways= await service.getAll()
-        return res.render('catways/list', {catways:catways})
+        const catways= await service.getAll();
+        return res.render('catways/list', {catways:catways});
     } catch (error) {
-        return res.status(500).json(error)
+        return res.render('error',{error:error})
+    }
+}
+
+exports.getPage = async (req,res) => {
+    const id = req.params.id
+    try {
+        const catway = await service.getOneById(id);
+        if (!catway) {
+            return res.render('error', {error:{}});
+        }
+        return res.render('catways/page', {catway:catway});
+    } catch (error) {
+        return res.render('error',{error:error})
     }
 }
