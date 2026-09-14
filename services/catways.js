ad3b5073586647a1429ecb7950db823680684b3b
Theanael <theanael@gmail.com>
2026-09-14 11:01:02 +0200

    added catways add service

--- services/catways.js
+++ services/catways.js
@@ -9,3 +9,8 @@ exports.getOneById = async (id) => {
     const catway=await Catway.findById(id)
     return catway;
 }
+
+exports.add = async (newCatway) => {
+    const catway=await Catway.create(newCatway);
+    return catway;
+}
