8dd6c0af0a7dc5219731d77d88cdd58aa1852980
Theanael <theanael@gmail.com>
2026-09-14 11:32:09 +0200

    added catway API getOneById controller

--- controllers/api/catways.js
+++ controllers/api/catways.js
@@ -3,7 +3,6 @@ const service = require('../../services/catways')
 exports.getAll = async (req,res) => {
     try {
         const catways= await service.getAll()
-        console.log(catways)
         return res.status(200).json(catways)
     } catch (error) {
         return res.status(500).json(error)
@@ -11,3 +10,14 @@ exports.getAll = async (req,res) => {
     
 }
 
+exports.getOneById = async (req,res) => {
+    try {
+        const catway = await service.getOneById(req.params.id)
+        if (!catway){
+            return res.status(404).json({message:"catway not found"})
+        }
+        return res.status(200).json(catway)
+    } catch (error) {
+        return res.status(500).json(error)
+    }
+}
