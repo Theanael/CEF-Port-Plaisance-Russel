347f1b05b142a9b0c9cab305bfe165fe31e212f8
Theanael <theanael@gmail.com>
2026-09-14 10:52:54 +0200

    added catways getOneById service

--- services/catways.js
+++ services/catways.js
@@ -4,3 +4,8 @@ exports.getAll = async () => {
     const catways=await Catway.find();
     return catways;
 }
+
+exports.getOneById = async (id) => {
+    const catway=await Catway.findById(id)
+    return catway;
+}
