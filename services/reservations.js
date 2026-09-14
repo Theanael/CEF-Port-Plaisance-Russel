fb64f9a6156b93d0f9505d530b29d3770c65cdab
Theanael <theanael@gmail.com>
2026-09-14 15:43:54 +0200

    Added reservation getAllByCatway service

--- services/reservations.js
+++ services/reservations.js
@@ -5,3 +5,9 @@ exports.getAll = async () =>{
     const reservations = Reservation.find();
     return reservations;
 } 
+
+exports.getAllByCatway = async (catwayNumber) => {
+    
+    const reservations = Reservation.find({catwayNumber:catwayNumber});
+    return reservations;
+}
