195dbe7f99c79037b04f59be6a8ffa850957fd17
Theanael <theanael@gmail.com>
2026-09-14 11:45:13 +0200

    added catway API updateState controller

--- controllers/api/catways.js
+++ controllers/api/catways.js
@@ -11,8 +11,9 @@ exports.getAll = async (req,res) => {
 }
 
 exports.getOneById = async (req,res) => {
+    const id = req.params.id
     try {
-        const catway = await service.getOneById(req.params.id)
+        const catway = await service.getOneById(id)
         if (!catway){
             return res.status(404).json({message:"catway not found"})
         }
@@ -23,16 +24,30 @@ exports.getOneById = async (req,res) => {
 }
 
 exports.add = async (req,res) => {
-
     const newCatway= {
         catwayNumber:req.body.catwayNumber,
         catwayType:req.body.catwayType,
         catwayState:req.body.catwayState
     }
-
     try {
         const catway =await service.add(newCatway)
-        return res.status(200).json(catway)
+        return res.status(201).json(catway)
+    } catch (error) {
+        return res.status(500).json(error)
+    }
+}
+
+exports.updateState = async (req,res) => {
+    const id = req.params.id
+    const newState = req.body.catwayState
+    try {
+        const catway = await service.getOneById(id)
+        if (!catway){
+            return res.status(404).json({message:"catway not found"})
+        }
+        await service.updateState(id,newState)
+        return res.status(200).json("catwayState changed")
+
     } catch (error) {
         return res.status(500).json(error)
     }
