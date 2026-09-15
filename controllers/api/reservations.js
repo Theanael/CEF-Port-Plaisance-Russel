f8bcf7281331ab1efbf8db74b4b6d48b7607139d
Theanael <theanael@gmail.com>
2026-09-15 09:59:50 +0200

    Added getById reservation controller

--- controllers/api/reservations.js
+++ controllers/api/reservations.js
@@ -3,18 +3,42 @@ const reservationService = require('../../services/reservations')
 
 exports.getAllByCatway = async(req,res) => {
     const catwayId=req.params.id
+
     try {
         const catway = await catwayService.getOneById(catwayId);
-        console.log(req.params)
         if (!catway){
             return res.status(404).json({message:"catway not found"})
         }
 
         const reservations = await reservationService.getAllByCatway(catway.catwayNumber);
+        
         return res.status(200).json(reservations)
 
     } catch (error) {
 
         return res.status(500).json("error")
+    }
+}
+
+
+
+exports.getOneById = async (req,res) => {
+    const catwayId=req.params.id
+    const reservationId=req.params.idReservation
+
+    try {
+        const catway = await catwayService.getOneById(catwayId);
+        if (!catway){
+            return res.status(404).json({message:"catway not found"})
+        }
+
+        const reservation = await reservationService.getOneById(reservationId);
+        if (!reservation || reservation.catwayNumber!=catway.catwayNumber){
+            return res.status(404).json({message:"reservation not found"})
+        }
+
+        return res.status(200).json(reservation)
+    } catch (error) {
+        return res.status(500).json(error)
     }
 }
