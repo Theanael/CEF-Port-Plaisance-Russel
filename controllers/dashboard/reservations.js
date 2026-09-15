107b8e3a49d1b58689b4b85280328867fc0bf299
Theanael <theanael@gmail.com>
2026-09-15 14:05:56 +0200

    Added update controller +minor routes fix

--- controllers/dashboard/reservations.js
+++ controllers/dashboard/reservations.js
@@ -19,7 +19,7 @@ exports.getPage = async (req,res) => {
             }
     
             // on renvoie la réservation.
-                return res.render('reservations/page',{reservation:reservation})
+                return res.render('reservations/page',{reservation:reservation,catway:catway})
         } catch (error) {
             return res.render('error',{message:error.message,error:error})
         }
@@ -60,6 +60,42 @@ exports.add = async (req,res) => {
 
 
 
+    } catch (error) {
+        return res.render('error',{message:error.message,error:error})
+    };
+}
+
+exports.update = async (req,res) => {
+    console.log('update')
+    const catwayId=req.params.id
+    const reservationId=req.params.idReservation
+    // Je pars du principe qu'une réservation ne peut pas changer de catway
+    // pour des raisons de praticité 
+    const changes={
+        clientName:req.body.clientName,
+        boatName:req.body.boatName,
+        startDate:req.body.startDate,
+        endDate:req.body.endDate,
+    }
+
+    try {
+         // on vérifie que le catway existe et on le récupère
+        const catway = await catwayService.getOneById(catwayId);
+        if (!catway){
+                return res.render('error', {message:"Catway Not Found",error:{status:404}});
+        }
+
+        // on vérifie que la réservation existe et est dans le bon catway et on la récupère 
+        const reservation = await reservationService.getOneById(reservationId);
+        if (!reservation || reservation.catwayNumber!=catway.catwayNumber){
+                return res.render('error', {message:"Reservation Not Found",error:{status:404}});
+        }
+
+        // 
+        await reservationService.update(reservationId,changes)
+        console.log('boop')
+        return res.redirect('/catways/'+catwayId+'/reservations/'+reservationId+'/page')
+        
     } catch (error) {
         return res.render('error',{message:error.message,error:error})
     };
--- routes/catways.js
+++ routes/catways.js
@@ -4,10 +4,11 @@ const router = express.Router();
 const APIcontroller = require('../controllers/api/catways');
 const dashboardController = require('../controllers/dashboard/catways');
 
+const reservationRoute= require('../routes/reservation')
 
 // Dashboard Routes
 router.get('/list',dashboardController.getList);
-router.get('/:id',dashboardController.getPage);
+router.get('/:id/page',dashboardController.getPage);
 router.get('/create',dashboardController.create);
 router.post('/add',dashboardController.add);
 router.get('/delete/:id',dashboardController.delete);
@@ -21,5 +22,6 @@ router.post('/',APIcontroller.add);
 router.put('/:id',APIcontroller.updateState);
 router.delete('/:id',APIcontroller.delete);
 
+router.use('/:id/reservations',reservationRoute)
 
 module.exports = router;
