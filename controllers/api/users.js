4eb8e92826e6cbd798e2731c8bfd74600262166c
Theanael <theanael@gmail.com>
2026-09-16 10:29:36 +0200

    Added getOneById user controller + fixed var names in getAll

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -3,8 +3,21 @@ const service = require('../../services/users')
 
 exports.getAll = async (req,res) => {
     try {
-        const catways= await service.getAll()
-        return res.status(200).json(catways)
+        const users= await service.getAll()
+        return res.status(200).json(users)
+    } catch (error) {
+        return res.status(500).json(error)
+    }
+}
+
+exports.getOneById = async (req,res) => {
+    const id = req.params.id
+    try {
+        const user = await service.getOneById(id)
+        if (!user){
+            return res.status(404).json({message:"user not found"})
+        }
+        return res.status(200).json(user)
     } catch (error) {
         return res.status(500).json(error)
     }
