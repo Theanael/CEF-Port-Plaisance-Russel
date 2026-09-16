fd5d0064c7d684fc129246f792340e6378075525
Theanael <theanael@gmail.com>
2026-09-16 10:14:34 +0200

    Added update user service

--- services/users.js
+++ services/users.js
@@ -1,16 +1,29 @@
 const User = require('../models/users');
 
 exports.getAll = async () => {
-    const users = User.find();
+    const users = await User.find();
     return users;
 }
 
 exports.getOneById = async (id) => {
-    const user = User.findById(id);
+    const user = await User.findById(id);
     return user;
 }
 
 exports.add = async (newUser) => {
-    const user = User.create(newUser);
+    const user = await User.create(newUser);
     return user;
+}
+
+exports.update = async(id,changes)=> {
+    const user = await User.findById(id);
+
+    if (user) {
+        Object.entries(changes).forEach(([key, field]) => {
+            if (field != '') {
+                user[key] = field;
+            }
+        });
+        await user.save();
+    }
 }
