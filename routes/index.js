ed6d0a5dff59324fadb991297b7d5b74ed7cfa47
Theanael <theanael@gmail.com>
2026-09-16 14:59:47 +0200

    Added authenticate service and login API controller

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const userAPIController= require('../controllers/api/users')
+
 const catwayRoutes= require('./catways')
 const userRoutes = require('./users')
 
@@ -8,6 +10,8 @@ const userRoutes = require('./users')
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/login',userAPIController.login)
 
 router.use('/catways', catwayRoutes)
 router.use('/user', userRoutes)
